efbcecaee76ec60873f28daa7db47fb89c71ca57
Kh1ed <kkhaled8600@gmail.com>
2022-10-27 13:14:46 +0300

    publish

---
+++ src/main/java/com/example/groupproject404/GroupProject404Application.java
@@ -0,0 +1,13 @@
+package com.example.groupproject404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProject404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProject404Application.class, args);
+	}
+
+}
