35670602fa6d0e2e9c45a3428d83eb5388055044
anasayb <anasayb@users.noreply.github.com>
2022-11-05 00:14:19 +0300

    define paths services and implement them
    - added CRUD services for path
    - define other services for path
    - modify Path entity
    - add the Path controller

---
+++ src/main/java/sa/edu/kau/stu/drone_system/controller/StringResponseWrapper.java
@@ -0,0 +1,22 @@
+package sa.edu.kau.stu.drone_system.controller;
+
+public class StringResponseWrapper {
+	
+	private String response;
+
+	
+	public StringResponseWrapper(String response) {
+		this.response = response;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+	
+
+}
