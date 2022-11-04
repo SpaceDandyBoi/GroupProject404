07e126e8d504bcda800fbd7b691dec68ba36914b
Sal Bakraa <SalBakraa@gmail.com>
2022-11-04 21:19:52 +0300

    refactor: fix formatting

--- src/main/java/sa/edu/kau/stu/drone_system/service/DroneService.java
+++ src/main/java/sa/edu/kau/stu/drone_system/service/DroneService.java
@@ -12,13 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class DroneService implements IDroneService {
 	@Autowired
-    DroneDAO _droneDao;
-
+	DroneDAO _droneDao;
 
 	@Override
 	public void addDrone(Drone drone) {
 		_droneDao.save(drone);
-
 	}
 
 	@Override
@@ -28,43 +26,38 @@ public class DroneService implements IDroneService {
 
 	@Override
 	public Drone getDroneInfo(Long id) {
-
 		Optional<Drone> drone = _droneDao.findById(id);
-		if(drone.isPresent()) {
+		if (drone.isPresent()) {
 			return drone.get();
-		}else {
+		} else {
 			return null;
 		}
 	}
 
 	@Override
 	public boolean deleteDrone(Long id) {
-
-			Optional<Drone> drone = _droneDao.findById(id);
-			if(drone.isPresent()) {
-				_droneDao.delete(drone.get());
-				return true;
-			}else {
-				return false;
-			}
-
+		Optional<Drone> drone = _droneDao.findById(id);
+		if (drone.isPresent()) {
+			_droneDao.delete(drone.get());
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean updateDrone(Long id, Drone droneToupdate) {
-
-			if(_droneDao.findById(id).isPresent()) {
-				Drone drone = _droneDao.findById(id).get();
-				drone.setName(droneToupdate.getName());
-				drone.setModel(droneToupdate.getModel());
-				drone.setMass(droneToupdate.getMass());
-				drone.setBatteryCap(droneToupdate.getBatteryCap());
-				drone.setBattPerc(droneToupdate.getBattPerc());
-				_droneDao.save(drone);
-				return true;
-			}else {
-				return false;
-			}
-
+		if (_droneDao.findById(id).isPresent()) {
+			Drone drone = _droneDao.findById(id).get();
+			drone.setName(droneToupdate.getName());
+			drone.setModel(droneToupdate.getModel());
+			drone.setMass(droneToupdate.getMass());
+			drone.setBatteryCap(droneToupdate.getBatteryCap());
+			drone.setBattPerc(droneToupdate.getBattPerc());
+			_droneDao.save(drone);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
