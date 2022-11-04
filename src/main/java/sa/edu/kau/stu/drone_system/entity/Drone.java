6ede443a056582bb442181a4b27383d5f0281b66
Sal Bakraa <SalBakraa@gmail.com>
2022-11-04 21:19:52 +0300

    refactor: fix formatting

--- src/main/java/sa/edu/kau/stu/drone_system/entity/Drone.java
+++ src/main/java/sa/edu/kau/stu/drone_system/entity/Drone.java
@@ -1,19 +1,14 @@
 package sa.edu.kau.stu.drone_system.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
 public class Drone {
-
-
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String Name;
 	private String model;
