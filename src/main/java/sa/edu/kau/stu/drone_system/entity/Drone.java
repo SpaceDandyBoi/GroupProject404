464099a5c1bb3399e3d1c1f69134a7d9f90e626e
Sal Bakraa <SalBakraa@gmail.com>
2022-11-05 19:59:22 +0300

    refactor!: Store path data inside each drone entity and convert csv to json

--- src/main/java/sa/edu/kau/stu/drone_system/entity/Drone.java
+++ src/main/java/sa/edu/kau/stu/drone_system/entity/Drone.java
@@ -1,42 +1,47 @@
 package sa.edu.kau.stu.drone_system.entity;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 
 public class Drone {
 	@Id
-	private Long id;
-	private String Name;
+	private String id;
+	private String name;
 	private String model;
 	private double mass;
 	private int batteryCapacity;
 	private double batteryPercentage;
+	private List<Coord> path;
 
 	public Drone() {
 	}
 
-	public Drone(Long id, String name, String model, double mass, int batteryCap, double battPerc) {
+	public Drone(String id, String name, String model, double mass, int batteryCap, double battPerc,
+			List<Coord> path) {
 		this.id = id;
-		Name = name;
+		this.name = name;
 		this.model = model;
 		this.mass = mass;
 		this.batteryCapacity = batteryCap;
 		this.batteryPercentage = battPerc;
+		this.path = path;
 	}
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
 	public String getName() {
-		return Name;
+		return name;
 	}
 
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
 
 	public String getModel() {
@@ -69,5 +74,62 @@ public class Drone {
 
 	public void setBatteryPercentage(double battPerc) {
 		this.batteryPercentage = battPerc;
+	}
+
+	public List<Coord> getPath() {
+		return path;
+	}
+
+	public void setPath(List<Coord> path) {
+		this.path = path;
+	}
+}
+
+class Coord {
+	private int x;
+	private int y;
+	private int z;
+	private int time;
+
+	public Coord() {
+	}
+
+	public Coord(int x, int y, int z, int time) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.time = time;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
 	}
 }
--- src/main/java/sa/edu/kau/stu/drone_system/repository/PathRepository.java
+++
@@ -1,8 +0,0 @@
-package sa.edu.kau.stu.drone_system.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import sa.edu.kau.stu.drone_system.entity.Path;
-
-public interface PathRepository extends MongoRepository<Path, Long> {
-}
