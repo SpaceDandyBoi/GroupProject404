c41562aac35f25439bf0bb2d73163fa8f1776aa4
Sal Bakraa <SalBakraa@gmail.com>
2022-11-06 18:58:00 +0300

    refactor!(project): Move project to a subdirectory

--- DroneSystem/src/main/java/sa/edu/kau/stu/drone_system/DroneSystemApplication.java
+++ DroneSystem/src/main/java/sa/edu/kau/stu/drone_system/DroneSystemApplication.java
@@ -3,6 +3,7 @@ package sa.edu.kau.stu.drone_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DroneSystemApplication {
 	public static void main(String[] args) {
