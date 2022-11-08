0c2c49c0f968f947ff822b4c87962da43d2e51a3
Sal Bakraa <SalBakraa@gmail.com>
2022-11-08 17:33:45 +0300

    refactor: remove unused imports

--- DroneSystem/src/main/java/sa/edu/kau/stu/drone_system/controller/DroneController.java
+++ DroneSystem/src/main/java/sa/edu/kau/stu/drone_system/controller/DroneController.java
@@ -5,15 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import sa.edu.kau.stu.drone_base_library.entity.Drone;
 import sa.edu.kau.stu.drone_database_service.service.IDroneService;
